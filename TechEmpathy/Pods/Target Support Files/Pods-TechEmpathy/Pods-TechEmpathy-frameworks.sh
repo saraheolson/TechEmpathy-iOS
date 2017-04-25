ba986ef33c8981ce35df477726cfbb3b0ce7990d
Sarah Olson <solson@trello.com>
2017-04-24 20:21:22 -0500

    Added PINRemoteImage for downloading/caching images.

--- TechEmpathy/Pods/Target Support Files/Pods-TechEmpathy/Pods-TechEmpathy-frameworks.sh
+++ TechEmpathy/Pods/Target Support Files/Pods-TechEmpathy/Pods-TechEmpathy-frameworks.sh
@@ -89,12 +89,18 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/FLAnimatedImage/FLAnimatedImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PINCache/PINCache.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PINRemoteImage/PINRemoteImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/FLAnimatedImage/FLAnimatedImage.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GTMSessionFetcher/GTMSessionFetcher.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GoogleToolboxForMac/GoogleToolboxForMac.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PINCache/PINCache.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/PINRemoteImage/PINRemoteImage.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
---
+++ TechEmpathy/TechEmpathy/TechEmpathy-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  TechEmpathy-Bridging-Header.h
+//  TechEmpathy
+//
+//  Created by Sarah Olson on 4/24/17.
+//  Copyright © 2017 SarahEOlson. All rights reserved.
+//
+
+#import <PINRemoteImage/PINRemoteImage.h>
